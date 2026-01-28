520d3f47172af224bfef394a2d4e45dad35cb02f
Sanimgul Serikqyzy <ssanimkz@gmail.com>
2026-01-28 17:12:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-
 import streamlit as st
+import joblib
+import pandas as pd
+import numpy as np
 
-st.title("Predicting the Risk of Elevated Cholesterol Using Machine Learning Models")
-st.write("Приложение успешно запущено")
-st.button("Test button")
+st.title("Predicting the Risk of Elevated Cholesterol")
+
+# загрузка модели
+model = joblib.load("rf_cholesterol_model.pkl")
+st.success("Модель успешно загружена")
+
+st.write("Тестовое предсказание (проверка подключения модели)")
+
+if st.button("Run test prediction"):
+    # создаём фиктивную строку с нужным числом признаков
+    X_dummy = pd.DataFrame(
+        np.zeros((1, model.n_features_in_))
+    )
+
+    proba = model.predict_proba(X_dummy)[0, 1]
+    st.write(f"Вероятность высокого холестерина: **{proba:.3f}**")
